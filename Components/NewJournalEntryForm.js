9ffa28d8a2a20c84fcd9b1f99877f095e4b89b6d
unxpctederr <mattster606@gmail.com>
2020-09-28 16:05:23 +1000

    add slider

--- Components/NewJournalEntryForm.js
+++ Components/NewJournalEntryForm.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { View } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import {withTheme, Button, TextInput, Title } from "react-native-paper";
-
+import Slider from '@react-native-community/slider';
+import * as Haptics from 'expo-haptics';
 
 const NewJournalEntryForm = (props) => {
     const themeColors = props.theme.colors;
     const { control, handleSubmit, errors, formState } = useForm({
         mode: "onChange"
     });
-    const onSubmit = (d) => {console.log(d)}
-
-    console.log(errors)
+    const onSubmit = data => console.log(data);
 
     return(
         <View>
+
             <Title>
                 New Entry
             </Title>
@@ -25,7 +25,8 @@ const NewJournalEntryForm = (props) => {
                 defaultValue={""}
                 render={(props) =>
                     <TextInput
-                        mode={"flat"}
+                        {...props}
+                        mode={"outlined"}
                         label={"Title"}
                         onChangeText={(value) => {props.onChange(value)}}
                         value={props.value}
@@ -33,14 +34,15 @@ const NewJournalEntryForm = (props) => {
                     />}
             />
 
+
             <Controller
                 name="description"
                 control={control}
-                rules={{required: true}}
                 defaultValue={""}
                 render={(props) =>
                     <TextInput
-                        mode={"flat"}
+                        {...props}
+                        mode={"outlined"}
                         label={"Description"}
                         multiline={true}
                         error={errors.description}
@@ -49,7 +51,29 @@ const NewJournalEntryForm = (props) => {
                     />}
             />
 
+            <Controller
+                name="dreamRating"
+                control={control}
+                rules={{required: true}}
+                defaultValue={5}
+                render={(props) =>
+                   <Slider
+                       {...props}
+                       minimumValue={1}
+                       maximumValue={10}
+                       step={1}
+                       onValueChange={(value) => {
+                           Haptics.selectionAsync()
+                           props.onChange(value)
+                       }}
+                       value={props.value}
+                       thumbTintColor={themeColors.text}
+                       minimumTrackTintColor={themeColors.primary}
+                   />}
+            />
+
             <Button
+                type={"submit"}
                 mode="contained"
                 onPress={handleSubmit(onSubmit)}>
                 Submit
